c9db180a29382e1cb480973de9430be55c69f0e6
pilaarr <pmoreno.ieu2021@student.ie.edu>
2023-01-26 10:59:58 +0100

    This is what we did in the third session

---
+++ print_examples.py
@@ -0,0 +1,6 @@
+print("This is a text", "another text", end="[I want this at the end]\n") # instead of ending the sentence with an enter (the default), it ends with what I've written
+# the n inserts an enter after the print
+print("This is another")
+print("this", "can", "be", "written", "like", "this", 2+2)
+print("this", "can", "be", "written", "like", "this", 2+2, sep="! bob?") # sep is to define what separates each word
+# if you do control+a it selects everything, and control+/ it transforms everything into comments
